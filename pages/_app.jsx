23b8498a3cc4230a672a64ab3b8632bb856a5e87
Shmish <me@shmish.dev>
2023-01-06 10:45:11 -0500

    feat: Add basic page analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,7 @@
 import "/public/styles/globals.css"
 
 import font from "@next/font/local"
+import { Analytics } from '@vercel/analytics/react'
 import { ThemeProvider } from "next-themes"
 
 
@@ -21,6 +22,7 @@ export default function App({ Component, pageProps }) {
     <ThemeProvider attribute="class">
       <main className={fonts}>
         <Component {...pageProps} />
+        <Analytics />
       </main>
     </ThemeProvider>
   )
